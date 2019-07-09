00fd779d0a476aa128d3b5aca17d1aa9c0caa8d3
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2019-07-09 12:12:38 -0500

    fixes

--- classes/subpages/housekeeping.php
+++ classes/subpages/housekeeping.php
@@ -118,13 +118,15 @@ class Housekeeping extends Subpage
 						
 						if ( $inTable == 1)
 						{
-							$width = intval(100 / $r['custom_fields'][1130376522209440]);
+							$width = intval(100 / $this->tasks[$r]['custom_fields'][1130376522209440]);
 							$out .= "<td style='text-align:left; vertical-align:top; width:{$width}'>{$this->tasks[$r]['name']}{$this->tasks[$r]['html_notes']}</td>";
 						}
 						else
 						{
 							$out .= "<li>{$this->tasks[$r]['name']}{$this->tasks[$r]['html_notes']}</li>";
 						}
+						
+						$sectionItems += 1;
 					}
 				}
 			}
